9fc1a0583343329664ecff0f28da9ec30d771773
RohitKV1n0d <rohitvinod92@gmail.com>
2022-10-10 03:31:51 +0530

    dont knwo

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ ENV = 'prod'
 
 if ENV == 'dev' :
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
     app.config['SECRET_KEY'] = 'asdasdasdasdasdasdasdaveqvq34c'
 
 else:
